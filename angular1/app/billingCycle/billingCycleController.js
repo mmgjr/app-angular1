04852f82805c42090741400d9f360a3483c5e20e
Mucio M G Junior <mlk_mucio@hotmail.com>
2020-04-06 16:20:40 -0300

    Lista do Ciclo de pagamento

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -7,15 +7,25 @@
 
     function BillingCycleController($http, msgs){
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function(){
+            $http.get(url).then(function(resp){
+                vm.billingCycle = {}
+                vm.billingCycles = resp.data
+            })
+        }
+
 
         vm.create = function(){
-            const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(resp){
-                vm.billingCycle = {}
+                vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
             }).catch(function(resp){
                 msgs.addError(resp.data.errors)
             })
         }
+
+        vm.refresh()
     }
 })()
